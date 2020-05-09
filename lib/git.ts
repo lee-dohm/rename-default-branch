7311b94afd43de613cecbc53b6d345e32f48cf6e
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2020-05-09 15:12:44 -0700

    Add Git.inGitHubRepo

--- lib/git.ts
+++ lib/git.ts
@@ -8,6 +8,21 @@ async function execGit(args: string, options: child_process.ExecOptions) {
 }
 
 /**
+ * Determines if `dir` is inside a repository where `origin` points to GitHub.
+ *
+ * Returns `true` if `dir` is a GitHub repository; `false` otherwise.
+ */
+export async function inGitHubRepo(dir: string) {
+  try {
+    const { stdout } = await execGit('remote get-url origin', { cwd: dir })
+
+    return stdout.includes('github.com')
+  } catch (e) {
+    return false
+  }
+}
+
+/**
  * Determines if `dir` is inside a git repository.
  *
  * Returns `true` if `dir` is inside a git repository; `false` otherwise.
--- test/git.test.ts
+++ test/git.test.ts
@@ -14,4 +14,28 @@ describe('Git', () => {
       expect(await Git.inRepo(dir)).toBe(false)
     })
   })
+
+  describe('inGitHubRepo', () => {
+    it('returns true when origin points to a GitHub repository', async () => {
+      expect(await Git.inGitHubRepo(__dirname)).toBe(true)
+    })
+
+    it('returns false when not in a git repo', async () => {
+      const dir = path.resolve(__dirname, '..', '..')
+
+      expect(await Git.inGitHubRepo(dir)).toBe(false)
+    })
+
+    it('returns false when in a git repo with no origin', async () => {
+      const dir = path.resolve(__dirname, 'fixtures/repo-no-origin')
+
+      expect(await Git.inGitHubRepo(dir)).toBe(false)
+    })
+
+    it('returns false when in a git repo where origin does not point to GitHub', async () => {
+      const dir = path.resolve(__dirname, 'fixtures/repo-non-github-origin')
+
+      expect(await Git.inGitHubRepo(dir)).toBe(false)
+    })
+  })
 })
